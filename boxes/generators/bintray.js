a03cbb726c61ca9cae41c436246ebf72e98f2a2c
Colin Faulkingham <colinfaulkingham@Colins-MBP.indoff.com>
2025-12-03 10:00:40 -0600

    fix for bintray

--- boxes/generators/bintray.js
+++ boxes/generators/bintray.js
@@ -5,38 +5,56 @@ import { edges  } from '../edges.js';
 import { _TopEdge  } from '../lids.js';
 import { Color  } from '../Color.js';
 
-class BinFrontEdge extends Boxes {
-    __call__(length) {
-        let f = this.settings.front;
+class BinFrontEdge extends edges.BaseEdge {
+    constructor(boxes, settings) {
+        super(boxes, settings);
+        this.char = "B";
+    }
+    
+    draw(length, kw={}) {
+        // Access settings from the parent BinTray instance (passed as settings)
+        let f = this.settings.front || 0.4;
+        let sy = this.settings.sy || [50, 50];
+        let hi = this.settings.hi || 30;
+        let thickness = this.boxes.thickness;
+        
         let a1 = (Math.atan((f / (1 - f))) * 180 / Math.PI);
         let a2 = (45 + a1);
-        this.corner(-a1);
-        for (let [i, l] of this.settings.sy.entries()) {
-            this.edges["e"]((l * (((f ** 2) + ((1 - f) ** 2)) ** 0.5)));
-            this.corner(a2);
-            this.edges["f"](((l * f) * (2 ** 0.5)));
-            if (i < (this.settings.sy.length - 1)) {
+        this.boxes.corner(-a1);
+        for (let i = 0; i < sy.length; i++) {
+            let l = sy[i];
+            this.boxes.edges["e"].draw((l * (((f ** 2) + ((1 - f) ** 2)) ** 0.5)));
+            this.boxes.corner(a2);
+            this.boxes.edges["f"].draw(((l * f) * (2 ** 0.5)));
+            if (i < (sy.length - 1)) {
                 if (this.char === "B") {
-                    this.polyline(0, 45, (0.5 * this.settings.hi), -90, this.thickness, -90, (0.5 * this.settings.hi), (90 - a1));
+                    this.boxes.polyline(0, 45, (0.5 * hi), -90, thickness, -90, (0.5 * hi), (90 - a1));
                 }
                 else {
-                    this.polyline(0, -45, this.thickness, -a1);
+                    this.boxes.polyline(0, -45, thickness, -a1);
                 }
             }
             else {
-                this.corner(-45);
+                this.boxes.corner(-45);
             }
         }
     }
 
     margin() {
-        return (Math.max(this.settings.sy) * this.settings.front);
+        let sy = this.settings.sy || [50, 50];
+        let f = this.settings.front || 0.4;
+        return (Math.max(...sy) * f);
     }
 
 }
 
 export { BinFrontEdge };
+
 class BinFrontSideEdge extends BinFrontEdge {
+    constructor(boxes, settings) {
+        super(boxes, settings);
+        this.char = "b";
+    }
 }
 
 export { BinFrontSideEdge };
@@ -69,6 +87,11 @@ class BinTray extends Boxes {
         // Initialize hi (inner height) if not provided
         if (!this.hi) {
             this.hi = 30; // Default 30mm inner height
+        }
+        
+        // Initialize front if not provided
+        if (!this.front) {
+            this.front = 0.4; // Default 40% of bin height covered with slope
         }
         
         // Override with args
@@ -152,37 +175,49 @@ class BinTray extends Boxes {
         if (this.outside) {
             this.sx = this.adjustSize(this.sx);
             this.sy = this.adjustSize(this.sy);
-            this.h = this.adjustSize(this.h);
+            this.h = this.adjustSize(this.h, false); // e2=False in Python
         }
         let x = (this.sx.reduce((a, b) => a + b, 0) + (this.thickness * (this.sx.length - 1)));
         let y = (this.sy.reduce((a, b) => a + b, 0) + (this.thickness * (this.sy.length - 1)));
         let h = this.h;
+        let hi = this.hi = h; // Set hi = h as in Python
         let t = this.thickness;
         this.front = Math.min(this.front, 0.999);
+        
         this.addPart(new BinFrontEdge(this, this));
         this.addPart(new BinFrontSideEdge(this, this));
-        let angledsettings = this.edges["f"].settings;
-        angledsettings.setValues(this.thickness, true, {angle: 45});
-        const gEdges = angledsettings.edgeObjects(this, {chars: "gGH"});
-        const gEdge = gEdges[2]; // Get "G" edge
-        const gEdgeSmall = gEdges[0]; // Get "g" edge
         
-        // Now we can use the edges
-        let e = ["F", "f", new edges.SlottedEdge(this, this.sx.slice(0, -1).reverse(), gEdge), "f"];
+        // Create new angled finger joint settings (equivalent to copy.deepcopy + setValues)
+        let angledsettings = new edges.FingerJointSettings(this.thickness, true, {angle: 45});
+        const gEdges = angledsettings.edgeObjects(this, {chars: "gGH"});
+        const gEdge = gEdges[1]; // "G" edge (index 1 in "gGH")
+        const gEdgeSmall = gEdges[0]; // "g" edge (index 0 in "gGH")
+        
+        // outer walls
+        let e = ["F", "f", new edges.SlottedEdge(this, [...this.sx].reverse(), gEdge), "f"];
         this.rectangularWall(x, h, e, {callback: [this.xHoles.bind(this)], move: "right", label: "bottom"});
         this.rectangularWall(y, h, "FFbF", {callback: [this.yHoles.bind(this)], move: "up", label: "left"});
-        this.rectangularWall(y, h, "FFbF", {callback: [this.yHoles.bind(this)], label: "right"});
+        this.rectangularWall(y, h, "FFbF", {callback: [this.yHoles.bind(this)], move: "", label: "right"});
         this.rectangularWall(x, h, "Ffef", {callback: [this.xHoles.bind(this)], move: "left", label: "top"});
         this.rectangularWall(y, h, "FFBF", {move: "up only"});
+        
+        // floor
         this.rectangularWall(x, y, "ffff", {callback: [this.xSlots.bind(this), this.ySlots.bind(this), this.addMount.bind(this)], move: "right", label: "back"});
+        
+        // Inner walls - vertical
         for (let i = 0; i < (this.sx.length - 1); i += 1) {
             e = [new edges.SlottedEdge(this, this.sy, "f"), "f", "B", "f"];
-            this.rectangularWall(y, this.hi, e, {move: "up", label: `inner vertical ${i + 1}`});
+            this.rectangularWall(y, hi, e, {move: "up", label: `inner vertical ${i + 1}`});
         }
+        
+        // Inner walls - horizontal
         for (let i = 0; i < (this.sy.length - 1); i += 1) {
-            e = [new edges.SlottedEdge(this, this.sx, gEdgeSmall), "F", "e", "F"];
-            this.rectangularWall(x, this.hi, e, {move: "up", label: `inner horizontal ${i + 1}`});
+            e = [new edges.SlottedEdge(this, this.sx, "f", 0.5 * hi), "f",
+                 new edges.SlottedEdge(this, [...this.sx].reverse(), gEdge), "f"];
+            this.rectangularWall(x, hi, e, {move: "up", label: `inner horizontal ${i + 1}`});
         }
+        
+        // Front walls
         for (let i = 0; i < this.sy.length; i += 1) {
             e = [new edges.SlottedEdge(this, this.sx, gEdgeSmall), "F", "e", "F"];
             this.rectangularWall(x, ((this.sy[i] * this.front) * (2 ** 0.5)), e, {callback: [this.frontHoles(i).bind(this)], move: "up", label: `retainer ${i + 1}`});
